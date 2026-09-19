2d493872ad86a761008250aff01e003de1e9c74d
Neo Jun Wei <70019047+neojw1505@users.noreply.github.com>
2026-09-19 13:11:24 +0800

    Add: copy-linked-list-with-random-pointer - submission-1

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-1.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # pass 1: deep copy with old nodes pointing to new nodes 
+        #         and new nodes next and random ptrs set to None
+        old_to_new = {None: None} # Because both the .next and .random pointers can point to None, your code is guaranteed to look up None in the dictionary at some point 
+        curr = head
+        while curr:
+            old_to_new[curr] = Node(curr.val) # dont set next and random, default = None
+            curr = curr.next
+        
+        # pass 2: wire the new nodes next and random pointers by looking at the old list
+        curr = head
+        while curr:
+            old_node_random = curr.random
+            old_node_next = curr.next
+            new_node = old_to_new[curr]
+            new_node.next = old_to_new[old_node_next]
+            new_node.random = old_to_new[old_node_random]
+            curr = curr.next
+        
+        return old_to_new[head] 
+
