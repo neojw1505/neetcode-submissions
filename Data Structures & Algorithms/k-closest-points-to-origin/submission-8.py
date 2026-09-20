3c21a7d96a9640e0eab1f851eabb11c95f9f95aa
Neo Jun Wei <70019047+neojw1505@users.noreply.github.com>
2026-09-20 13:45:22 +0800

    Add: k-closest-points-to-origin - submission-8

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-8.py
@@ -0,0 +1,31 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        for i, (x,y) in enumerate(points):
+            dist = x**2 + y**2
+            points[i] = [dist, (x,y)]
+        self.quickSelect(points,0,len(points)-1,k)
+        for i, (x,y) in enumerate(points):
+            points[i] = points[i][1]
+        return points[:k]
+
+    def quickSelect(self, points, left, right, k):
+        # base 
+        if left >= right:
+            return
+        # define pivot
+        pivot = points[right]
+        i = left
+        for j in range(left, right):
+            if points[j][0] < points[right][0]:
+                points[i], points[j] = points[j], points[i]
+                i += 1
+        # swap pivot to correct separation point
+        points[right], points[i] = points[i], points[right]
+        # found k closest points
+        if i == k:
+            return 
+        elif i > k: # k in left pile 
+            self.quickSelect(points, left, i-1, k) # search left pile
+        else:
+            self.quickSelect(points, i+1, right, k) # search right pile
+
