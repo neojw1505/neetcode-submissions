e77dacc790910a0e94c0e9ee261e57c560a808f5
Neo Jun Wei <70019047+neojw1505@users.noreply.github.com>
2026-09-21 22:14:10 +0800

    Add: dijkstra - submission-0

---
+++ Data Structures & Algorithms/dijkstra/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def shortestPath(self, n: int, edges: List[List[int]], src: int) -> Dict[int, int]:
+        src_to_dst_and_weight = collections.defaultdict(list)
+
+        for u,v,w in edges:
+            src_to_dst_and_weight[u].append((w,v))
+
+        min_heap = [(0,src)]
+        visited = set()
+        res = {}
+
+        while min_heap:
+            weight, dst = heapq.heappop(min_heap)
+
+            if dst in visited: # already found shortest distance for vertex
+                continue 
+
+            visited.add(dst) # if not add to the vertex
+            res[dst] = weight
+
+            # add vertex neighbors 
+            for w1, d1 in src_to_dst_and_weight[dst]:
+                 if d1 not in visited:
+                    heapq.heappush(min_heap, (weight + w1, d1))
+        
+        return res
+
+
+            
+
+
+        
