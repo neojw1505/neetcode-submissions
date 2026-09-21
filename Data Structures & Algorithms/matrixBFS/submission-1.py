7dde2bab3b0562d4b83c501ac3d0f2840c995980
Neo Jun Wei <70019047+neojw1505@users.noreply.github.com>
2026-09-21 20:28:36 +0800

    Add: matrixBFS - submission-1

---
+++ Data Structures & Algorithms/matrixBFS/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        # BFS standard algo
+        if not grid:
+            return 0
+
+        if grid[0][0] == 1 or grid[ROWS-1][COLS-1] == 1:
+            return -1
+
+        length = 0
+        queue = collections.deque([(0,0)])
+        visited = {(0,0)}
+
+
+        while queue:
+            level_size = len(queue)
+            for _ in range(level_size):
+                r,c = queue.popleft() # pop from queue
+
+                # success
+                if r == ROWS - 1 and c == COLS - 1:
+                    return length
+
+                # append all 4 directions neighbours
+                directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+                for dr,dc in directions:
+                    nr,nc = r+dr, c+dc
+                    if 0 <= nr < ROWS and 0 <= nc < COLS and grid[nr][nc] == 0: # in-bound
+                        if (nr, nc) not in visited:
+                            queue.append((nr,nc))
+                            visited.add((nr,nc))
+            length += 1
+
+        return -1
