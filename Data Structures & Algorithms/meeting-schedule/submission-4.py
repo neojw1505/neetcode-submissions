8e93f8927749cf00f009386e9e145e6aa4de1cd5
Neo Jun Wei <70019047+neojw1505@users.noreply.github.com>
2026-09-23 23:08:07 +0800

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.py
@@ -0,0 +1,26 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals:
+            return True
+
+        intervals.sort(key=lambda x:x.start)
+        prev_start, prev_end = intervals[0].start, intervals[0].end
+        
+        for i in range(1, len(intervals)):
+            curr_start, curr_end = intervals[i].start, intervals[i].end
+            # overlap
+            if prev_end > curr_start:
+                return False
+            # no overlap
+            else:
+                prev_end = curr_end 
+        
+        return True
