8b24e624e53c3295af15a8306e8acb68a98451d7
Neo Jun Wei <70019047+neojw1505@users.noreply.github.com>
2026-09-24 11:58:12 +0800

    Add: singlyLinkedList - submission-3

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-3.py
@@ -0,0 +1,52 @@
+class ListNode:
+    
+    def __init__(self, val=-1, next=None):
+        self.val = val
+        self.next = next 
+
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = ListNode()
+        self.tail = self.head
+    
+    def get(self, index: int) -> int:
+        curr = self.head.next
+        for _ in range(index):
+            if curr:
+                curr = curr.next
+        return -1
+
+    def insertHead(self, val: int) -> None:
+        new_node = ListNode(val)
+        new_node.next = self.head.next 
+        self.head.next = new_node 
+        if not new_node.next:
+            self.tail = new_node
+
+    def insertTail(self, val: int) -> None:
+        self.tail.next = ListNode(val)
+        self.tail = self.tail.next 
+
+    def remove(self, index: int) -> bool:
+        curr = self.head
+        i = 0
+        for _ in range(index):
+            if curr:
+                curr = curr.next
+        if curr and curr.next:
+            if curr.next == self.tail:
+                self.tail = curr
+            curr.next = curr.next.next
+            return True
+        return False
+
+    def getValues(self) -> List[int]:
+        curr = self.head.next
+        res = []
+        while curr:
+            res.append(curr.val)
+            curr = curr.next 
+        return res
+        
