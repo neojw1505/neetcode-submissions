49730e7b21faa83831da3674014122090272ec8a
Neo Jun Wei <70019047+neojw1505@users.noreply.github.com>
2026-09-26 20:05:51 +0800

    Add: longest-increasing-subsequence - submission-10

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-10.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums: # no numbers -> LIS = 0
+            return 0 
+        board = []
+        for x in nums:
+            left, right = 0, len(board) # not len(board)-1 because the ans can be an index out of the board
+            while left < right:
+                mid = (left + right) // 2
+                if board[mid] >= x: # find first num bigger than x, potential slot to replace with x
+                    right = mid
+                else:
+                    left = mid + 1 # value too small, look right
+            idx = left
+            if idx == len(board): # APPEND
+                board.append(x)
+            else:
+                board[left] = x # REPLACE 
+            return len(board)
+    
+    # T:O(nlogk) S:O(k)
+
+
