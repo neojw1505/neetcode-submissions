33f951f6758588ea01dc04aaa23653a8e6418a4c
Neo Jun Wei <70019047+neojw1505@users.noreply.github.com>
2026-09-26 18:37:31 +0800

    Add: longest-increasing-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # dp[i] represents the length of the longest increasing subsequence that ends exactly at index i
+        n = len(nums)
+        dp = [1] * n # every char have at least length 1
+        dp[0] = 1
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return dp[-1]
