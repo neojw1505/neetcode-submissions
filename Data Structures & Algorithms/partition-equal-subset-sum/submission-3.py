c0cf75ff950354570af7a1a50bbeceb8517b7f8f
Neo Jun Wei <70019047+neojw1505@users.noreply.github.com>
2026-09-26 20:56:17 +0800

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2: return False # odd value cannot be split evenly
+
+        target = sum(nums) // 2
+        dp = set()
+
+        for i in range(len(nums)):
+            new_dp = set(dp)
+            for t in dp:
+                new_sum = t+nums[i]
+                if new_sum == target:
+                    return True
+                elif new_sum < target:
+                    new_dp.add(new_sum)
+            dp = new_dp
+        return False
+
+
+            
