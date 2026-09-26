e49a5917e68356fe350d7d1af08d316bb93d97e5
Neo Jun Wei <70019047+neojw1505@users.noreply.github.com>
2026-09-26 23:31:55 +0800

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+
+        dp = [False] * n
+        dp[n-1] = True
+       
+        for i in range(n-2,-1,-1):
+            furthest_jump = min(n-1, i + nums[i])
+            for j in range(i+1, furthest_jump + 1):
+                if dp[j] == True:
+                    dp[i] = True
+                    break
+        return dp[0] == True
+
+            
